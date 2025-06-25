e1a1f0d51ec81353ed28afdae0c3a03b7cef91c8
Felipe Giannetti <felipe.fontenelle@sga.pucminas.br>
2025-06-25 08:51:38 -0300

    avaliacao caminho

--- codigo/public/assets/js/eventosjs/avaliacao.js
+++ codigo/public/assets/js/eventosjs/avaliacao.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const API_URL = 'http://127.0.0.1:3000';
+    const API_URL = '';
     let usuarioLogado = null;
     let notaSelecionada = 0;
 
